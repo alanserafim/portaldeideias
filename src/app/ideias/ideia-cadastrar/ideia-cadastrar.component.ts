72a2ae6754bd7e8769a7ae2ca51b301aaa14e2de
alanserafim <alan.serafim@fatec.sp.gov.br>
2023-02-03 19:37:42 -0300

    ideias module

---
+++ src/app/ideias/ideia-cadastrar/ideia-cadastrar.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ideia-cadastrar',
+  templateUrl: './ideia-cadastrar.component.html',
+  styleUrls: ['./ideia-cadastrar.component.scss'],
+})
+export class IdeiaCadastrarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
---
+++ src/app/ideias/ideia-listar/ideia-listar.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ideia-listar',
+  templateUrl: './ideia-listar.component.html',
+  styleUrls: ['./ideia-listar.component.scss'],
+})
+export class IdeiaListarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
